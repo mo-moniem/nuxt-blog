ecdf376941932e04308a3b83cec7dd728486e750
mo-moniem <mohamedabdelmoniem101@gmail.com>
2021-10-19 16:58:33 +0200

    ep7

---
+++ pages/Blogs/_id.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <h1>{{$route.params.id}}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "_id",
+  validate({params}){
+      return !isNaN(params.id);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
