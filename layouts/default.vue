bf1b460653bfef57d6c20268f653406956b73db7
mo-moniem <mohamedabdelmoniem101@gmail.com>
2021-10-19 17:09:13 +0200

    EP8

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <header>
+    <h1>header</h1>
+  </header>
+  <Nuxt/>
+  <footer>
+    <h4>footer</h4>
+  </footer>
+</div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
